502d1576d5a9db248cd9fa244c0411f7090495f9
maBurger <homeburger@freenet.de>
2015-01-25 12:10:44 +0100

    Button included on arduino pin 4. Library for Button debounce included. Status machine concept included for the update of the strip leds.

--- ColorClock.ino
+++ ColorClock.ino
@@ -1,13 +1,30 @@
+#include <ClickButton.h>
 #include <TimerOne.h>
 #include <Adafruit_NeoPixel.h>
 
 #define PIN 6
 #define LICHTSENSOR A3
+#define BUTTONPIN 4
 
 uint8_t Sekunden = 50, Minuten = 2, Stunden = 1;
-uint8_t striche[12] = {0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55};
+uint8_t striche[12] = { 
+  0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55};
 uint8_t isrCounter = 0; 
 uint8_t minBrightness = 30;
+
+#define TIME_RUN_MODE 10
+#define TIME_UPDATE_MODE 11
+#define MENU_MODE 20
+#define RAINBOW_MODE 30
+uint8_t displayStatus = TIME_UPDATE_MODE;
+
+uint8_t menuItem = 0;
+uint8_t menuArray[4][3] = {
+  {59, 0, 1},
+  {14, 15, 16},
+  {29, 30, 31},
+  {44, 45, 46}
+};
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = Arduino pin number (most are valid)
@@ -18,7 +35,10 @@ uint8_t minBrightness = 30;
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
 
-uint32_t Sek_Color, Min_Color, Std_Color, stricheColor;
+// Click Button Initalize
+ClickButton button1(BUTTONPIN, LOW, CLICKBTN_PULLUP);
+
+uint32_t Sek_Color, Min_Color, Std_Color, stricheColor, menuColor;
 
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
 // pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
@@ -27,34 +47,87 @@ uint32_t Sek_Color, Min_Color, Std_Color, stricheColor;
 
 void setup() {
   pinMode(LICHTSENSOR, INPUT);
-  
+
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
   Sek_Color = strip.Color(0, 255, 0); // Green
   Min_Color = strip.Color(0, 0, 255); // Blue
   Std_Color = strip.Color(255, 0, 0); // Red
   stricheColor = strip.Color(20, 20, 20);
-  
+  menuColor = strip.Color(0, 255, 0);
+
   updateTime();
-  
-  Timer1.initialize(250000); // set a timer 250 milli seconds 
+
+  Timer1.initialize(250000); // set a timer 250 milli seconds Timer will fire 4 times in a second
   Timer1.attachInterrupt( timerIsr ); // attach the service routine here
-  
+
+  // Setup button timers (all in milliseconds / ms)
+  // (These are default if not set, but changeable for convenience)
+  button1.debounceTime   = 20;   // Debounce timer in ms
+  button1.multiclickTime = 250;  // Time limit for multi clicks
+  button1.longClickTime  = 1000; // time until "held-down clicks" register
+
   //debugging
-  //Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop(){
+  uint8_t i = 0;
+  button1.Update();
+
+  if( button1.clicks != 0 ){
+    // we need to build up a state machine
+    switch( displayStatus ){
+      case TIME_RUN_MODE:
+        // if( button1.clicks == 1 ) displayStatus = TIME_MODE;
+        if( button1.clicks == 2 ) displayStatus = RAINBOW_MODE;
+        if( button1.clicks == -3 ) displayStatus = MENU_MODE;
+        break;
+      case MENU_MODE:
+        if( button1.clicks == 1 ) menuItem++;
+        if( button1.clicks == -1 ) displayStatus = TIME_UPDATE_MODE;
+        //if( button1.clicks == -3 ) displayStatus = MENU_MODE;
+    }
+  }
+  updateStrip();
+}
+
+void updateStrip(){
+  switch( displayStatus ){
+    case TIME_RUN_MODE:
+      break;
+    case TIME_UPDATE_MODE:
+      updateTime();
+      displayStatus = TIME_RUN_MODE;
+      break;
+    case RAINBOW_MODE:
+      for( int i = 0; i < 2; i++){
+        rainbow(10);
+      }
+      displayStatus = TIME_UPDATE_MODE;
+      break;
+    case MENU_MODE:
+      clearStrip();
+      for( int i = 0; i < 60; i++){
+        strip.setPixelColor(i, stricheColor);
+      }
+      for( int i = 0; i < 3; i++){
+        strip.setPixelColor(menuArray[menuItem%4][i], menuColor);
+      }
+      strip.show();
+      break;
+  }
+  
 }
 
 void updateTime() {
   uint32_t help_Color;
-  
+
   clearStrip();
   setHours();
-  
+
   strip.setPixelColor(Sekunden, Sek_Color);
-  
+
   // Farbe für Minuten erzeugen
   help_Color = strip.getPixelColor(Minuten);
   help_Color |= Min_Color;
@@ -69,7 +142,7 @@ void updateTime() {
 
 void updateBrightness() {
   uint16_t lightRead;
-  
+
   lightRead = analogRead(LICHTSENSOR);
   //Serial.print(lightRead);
   //Serial.print("  ");
@@ -103,6 +176,34 @@ void minRun2(){
   }
 }
 
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  if(WheelPos < 85) {
+    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } 
+  else if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } 
+  else {
+    WheelPos -= 170;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
+
 void clearStrip(){
   for(uint8_t i = 0; i<strip.numPixels(); i++) {
     strip.setPixelColor(i, 0);
@@ -111,33 +212,37 @@ void clearStrip(){
 
 void timerIsr(){
   switch (isrCounter) {
-    case 0:
-      updateBrightness();
-      break;
-    case 1:
-      updateBrightness();
-      break;
-    case 2:
-      updateBrightness();
-      break;
-    case 3:
-      updateBrightness();
-      
-      //
-      Sekunden += 1;
-      if(Sekunden > 59){
-        Sekunden = 0;
-        Minuten += 1;
-        minRun1();
-        if(Minuten > 59){
-          Minuten = 0;
-          Stunden +=1;
-          if( Stunden > 23) Stunden = 0;
-        }
+  case 0:
+    updateBrightness();
+    break;
+  case 1:
+    updateBrightness();
+    break;
+  case 2:
+    updateBrightness();
+    break;
+  case 3:
+    updateBrightness();
+
+    //
+    Sekunden += 1;
+    if(Sekunden > 59){
+      Sekunden = 0;
+      Minuten += 1;
+      if( displayStatus == TIME_RUN_MODE ) minRun1();
+      if(Minuten > 59){
+        Minuten = 0;
+        Stunden +=1;
+        if( Stunden > 23) Stunden = 0;
       }
-      updateTime();
-      break;
+    }
+    // only when the clock is in TIME_RUN_MODE the update must be done
+    // especially important when in MENU_MODE
+    if(displayStatus == TIME_RUN_MODE) displayStatus = TIME_UPDATE_MODE;
+    break;
   }
   isrCounter += 1;
   if( isrCounter == 4 ) isrCounter = 0;
 }
+
+
--- README.md
+++ README.md
@@ -3,14 +3,16 @@ Colorful DIY clock with 60 Neo-LEDs.
 This is a project for the VHS Esslingen - Technikschule.
 In the project we are building up a real clock with one NEO RGB LED for each Minute or Second.
 
-Here a picture of a early version
+Here a picture of an early version
 ![Early Version](https://github.com/maBurger/ColorClock/blob/master/media/ColorClockV0.2.JPG)
 
 
 ## Hardware
 1. Standard arduino
-2. Neo RGB LED strip with 60 LED per Meter
-3. 40cm x 40cm MDF boards
+2. Neo RGB NEO LED strip with 60 LED per Meter
+3. Foto resistor and resistor
+4. Button
+5. 40cm x 40cm MDF boards
 
 ## Software
 Following requirements are needed
@@ -18,6 +20,8 @@ Following requirements are needed
 ***Libraries***
 * [TimerOne][TimerOne] for getting Timer Interupts for the seconds
 * [Adafruit Neo Pixel][AdaNeoPix] for driving the LED strip we have in use.
+* [ClickButton][ClickButton] for doing the Button debounce and double- and long click
 
 [AdaNeoPix]: https://github.com/adafruit/Adafruit_NeoPixel
 [TimerOne]: https://www.pjrc.com/teensy/td_libs_TimerOne.html
+[ClickButton]: https://code.google.com/p/clickbutton/
